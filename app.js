e4ecf8b33f1fccea0369795e4812c9ab7861fb4c
AndriiHamasa <andrii.khamaza@nure.ua>
2023-09-26 15:01:52 +0300

    create server

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+const sgMail = require("@sendgrid/mail");
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Подключение библиотеки dotenv для загрузки переменных окружения из файла .env
+require("dotenv").config();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.post("/submit-form", (req, res) => {
+  const { name, phone } = req.body;
+
+  const { SENDGRID_API_KEY } = process.env;
+
+  sgMail.setApiKey(SENDGRID_API_KEY);
+
+  const emailSG = {
+    to: "andrii.khamaza0@gmail.com",
+    from: "andrii.khamaza@nure.ua",
+    subject: "Test letter",
+    html: `<p><strong>name: ${name}, phone: ${phone}</strong></p>`,
+  };
+
+  sgMail
+    .send(emailSG)
+    .then(() => {
+      console.log("Email send success")
+      res.status(200).json({ message: 'All is good' });
+    })
+    .catch((error) => console.log(error.message));
+
+  
+
+  
+});
+
+app.listen(port, () => {
+  console.log(`Сервер запущен на порту ${port}`);
+});
